bf350434488976dec1d8d340d091b5f79eb58ac9
Tiriree <tirireekananuruk@172-16-216-146.DYNAPOOL.NYU.EDU>
2016-11-01 03:16:49 -0400

    new version, better lines, maybe broke oscillator

--- tiri_treatise_2.js/sketch.js
+++ tiri_treatise_2.js/sketch.js
@@ -4,16 +4,24 @@ var input;
 var osc;
 var fft;
 
+//declare variables
+var threshold;
+var levelOne;
+var levelTwo;
+var levelMax;
+var volume;
+
 function setup() {
-  createCanvas(800, 600);
+  //createCanvas(800, 600);
+  createCanvas(windowWidth, windowHeight);
   background(255);
 
   // Create an Audio input
   input = new p5.AudioIn();
 
   input.start();
-	//input.loop();
-  
+  //input.loop();
+
   // noise = new p5.Noise('brown');
   // noise.amp(0);
   // noise.start();
@@ -25,56 +33,71 @@ function setup() {
   // play with these numbers!!
   delay.process(input, .102, .7, 2300);
 
-   // play the noise with an envelope,
+  // play the noise with an envelope,
   // a series of fades ( time / value pairs )
   env = new p5.Env(.01, 0.2, .2, .1);
-  
+
   osc = new p5.Oscillator();
-	osc.setType('sine');
-	osc.freq(240);
+  osc.setType('sine');
+  osc.freq(240);
   osc.amp(0);
   osc.start();
   fft = new p5.FFT();
-  
+
+  // If the volume > 0.1,  a rect is drawn at a random location.
+  // The louder the volume, the larger the rectangle.
+  threshold = 0.1;
+  levelOne = 0.2;
+  levelTwo = 0.3;
+  levelMax = 0.4;
+  volume = 0;
+
 }
 
 function draw() {
   // Get the overall volume (between 0 and 1.0)
-  var volume = input.getLevel();
-  console.log(volume);
+  volume = input.getLevel();
+  //console.log(volume);
 
-  // If the volume > 0.1,  a rect is drawn at a random location.
-  // The louder the volume, the larger the rectangle.
-  var threshold = 0.1;
-	var levelOne = 0.2;
-	var levelTwo = 0.4;
-	var levelMax = 1;
 
-	
-  if (volume > threshold && volume < levelOne  ) {
+  //update levelMax
+  if (volume > levelMax) {
+    volumeMax = volume;
+  }
+
+
+  if (volume > threshold && volume < levelOne) {
     stroke(1);
-    rect(random(40, width), height/2, volume*100, volume*100);
-  } if (volume > levelOne && volume < levelTwo ) {
-		stroke(1);
-		ellipse(random(60, width), height/2, volume*50, volume*50);
-	} if (volume > levelTwo && volume < levelMax ) {
-		stroke(1);
-		line(random (width , width/2), random (height , height/2), volume*50, volume*50);
-	}
+    rect(random(40, width), height / 2, volume * 100, volume * 100);
+  }
+  if (volume > levelOne && volume < levelTwo) {
+    stroke(1);
+    ellipse(random(60, width), height / 2, volume * 50, volume * 50);
+  }
+  if (volume > levelTwo && volume < levelMax) {
+    stroke(1);
+    var aux1 = random(0, width);
+    var aux2 = random(0, height);
+    //console.log(aux1);
+    //console.log(aux2);
+    console.log(volume);
+    var aux3 = map(volume, levelTwo, levelMax, 0, width);
+    line(aux1, aux2, aux3, height / 2);
+  }
 
 
   // Graph the overall potential volume, w/ a line at the threshold
   var y = map(volume, 0, 1, height, 0);
   var ythreshold = map(threshold, 0, 1, height, 0);
-	
-	// Set the rate to a range between 0.1 and 4
+
+  // Set the rate to a range between 0.1 and 4
   // Changing the rate alters the pitch
-	var speed = map(mouseY, 0.1, height, 0, 2);
+  var speed = map(mouseY, 0.1, height, 0, 2);
   speed = constrain(speed, 0.01, 4);
   // input.rate(speed);
 
 
-// for fft
+  // for fft
   var freq = map(mouseX, 0, 800, 20, 15000);
   freq = constrain(freq, 1, 20000);
   osc.freq(freq);
@@ -93,11 +116,16 @@ function draw() {
 
 // only play sound when mouse is over canvas
 function mouseMoved() {
+  console.log("hey");
   var mX = mouseX;
-	var mY = mouseY;
+  var mY = mouseY;
   if (mX > 0 && mX < width && mY < height && mY > 0) {
     osc.amp(0.5, 0.2);
   } else {
     osc.amp(0, 0.2);
   }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
